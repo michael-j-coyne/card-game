66c45eeb8c5b50414ae17b682671b43d6bd12568
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2023-12-07 16:12:13 -0500

    Pre 4.2 update

---
+++ Background.gd
@@ -0,0 +1,16 @@
+class_name Background extends Sprite2D
+
+var dragging = false
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		var clicked_in_rect = get_rect().has_point(to_local(event.position))
+		
+		if event.pressed and clicked_in_rect:
+			if not dragging:
+				dragging = true
+				
+		if not event.pressed and dragging:
+			dragging = false
+		
+
---
+++ Card.gd
@@ -0,0 +1,13 @@
+extends MarginContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var card_size = size
+	
+func _input(event):
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
