46c757a503718f6057a4f0197a8a4f5de1250de6
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2023-12-13 16:45:03 -0500

    Implement card on-hover animation in hand
    Cards in hand that are hovered over with the mouse now enlarge and orient themselves so the user can see them clearly. This solution is incomplete but works well enough for now.

--- Card.gd
+++ Card.gd
@@ -1,5 +1,8 @@
 class_name Card extends Node2D
 
+signal mouse_entered(card: Card)
+signal mouse_exited(card: Card)
+
 func get_width() -> int:
 	var bg = get_node("Area2D/Background")
 	return bg.texture.get_width() * bg.scale.x
@@ -7,3 +10,9 @@ func get_width() -> int:
 func get_height() -> int:
 	var bg = get_node("Area2D/Background")
 	return bg.texture.get_height() * bg.scale.y
+
+func _on_area_2d_mouse_entered():
+	mouse_entered.emit(self)
+	
+func _on_area_2d_mouse_exited():
+	mouse_exited.emit(self)
--- Hand.gd
+++ Hand.gd
@@ -2,11 +2,6 @@ extends Node2D
 
 const CardScene = preload('res://Card.tscn')
 
-# TODO: Make cards spawn to either the left or right, where the 'deck' is located,
-# currently they spawn in the middle of the hand at the hand 'origin'
-
-# maybe store the curve somewhere and load it like 
-# const height_curve := preload("res://UI/resources/height_curve.tres") as Curve
 @export var horizontal_spread_curve :=  \
 	preload("res://UI/resources/horizontal_card_spread_curve.tres")
 @export var vertical_spread_curve := \
@@ -19,8 +14,14 @@ const CardScene = preload('res://Card.tscn')
 const MAX_SPREAD := 800.0
 const CARD_SPREAD_Y : float = 45.0
 const BASE_ROTATION_DEGREES = 6.5
-const ANIMATION_DURATION_SECONDS = 0.25
+const CARD_FAN_ANIMATION_DURATION_SECONDS = 0.25
+const CARD_HOVER_ANIMATION_DURATION_SECONDS = 0.125
 const CARD_SPAWN_POS = Vector2(1000, 0)
+const CARD_EXPANSION_FACTOR = Vector2(1.5, 1.5)
+const DEFAULT_CARD_Z_INDEX = 0
+const HOVERED_CARD_Z_INDEX = 1
+
+var hovered_cards : Array[Card] = []
 
 # Compute the horizontal space that the hand will occupy
 func _compute_card_spread_x(num_cards_in_hand: int, card_width: int) -> float:
@@ -39,25 +40,70 @@ func get_card_spread_x() -> float:
 func _compute_hand_ratio(card_index: int, hand_size: int) -> float:
 	if hand_size < 2: return 0.5
 	return float(card_index) / float(hand_size - 1)
-	
+
 func get_hand_ratio(card: Card) -> float:
 	return _compute_hand_ratio(card.get_index(), get_child_count())
 
 func _fan_cards():
 	if not get_children(): return
-	
+	const duration_seconds = CARD_FAN_ANIMATION_DURATION_SECONDS
 	var cards_in_hand := get_children()
 	var tween := create_tween()
 	tween.set_parallel(true)
-	
+
 	for card in cards_in_hand:
 		var pos := get_card_default_pos(card)
 		var rotation_amt := rotation_curve.sample(get_hand_ratio(card)) * BASE_ROTATION_DEGREES
-		tween.tween_property(card, "position", pos, ANIMATION_DURATION_SECONDS)
-		tween.tween_property(card, "rotation_degrees", rotation_amt, ANIMATION_DURATION_SECONDS)
+		tween.tween_property(card, "position", pos, duration_seconds)
+		tween.tween_property(card, "rotation_degrees", rotation_amt, duration_seconds)
 
 func _process(_delta : float) -> void:
 	_fan_cards()
+	animate_card_hover()
+
+# This function feels overworked to me. Let's see if we can split it up.
+func animate_card_hover() -> void:
+	var hovered_card := get_hovered_card()
+	if not hovered_card: return
+	
+	# This feels hacky to me... but it works for now.
+	var other_cards := get_children().filter(func(card): return card != hovered_card)
+	other_cards.map(scale_card_to_default_size)
+	
+	hovered_card.z_index = HOVERED_CARD_Z_INDEX
+	const duration_seconds = CARD_HOVER_ANIMATION_DURATION_SECONDS
+	
+	var y_pos = get_viewport().size.y - (0.5 * hovered_card.get_height() * CARD_EXPANSION_FACTOR.y)
+	var hovered_pos = to_local(Vector2(to_global(get_card_default_pos(hovered_card)).x, y_pos))
+	
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(hovered_card, "scale", CARD_EXPANSION_FACTOR, duration_seconds)
+	tween.tween_property(hovered_card, "rotation_degrees", 0, duration_seconds)
+	tween.tween_property(hovered_card, "position", hovered_pos, duration_seconds)
+
+func scale_card_to_default_size(card: Card) -> void:
+	if not card: return
+	var tween = create_tween()
+	tween.tween_property(card, "scale", Vector2(1, 1), 0.125) # .125 magic #
+
+# I may want to consider using an input signal instead of an entered
+# / exited signal because the input signal only triggers on unhandled input
+func _on_mouse_entered_card(card: Card):
+	hovered_cards.append(card)
+
+func _on_mouse_exited_card(card: Card):
+	scale_card_to_default_size(card)
+	card.z_index = DEFAULT_CARD_Z_INDEX
+	hovered_cards.erase(card)
+
+# sometimes this function returns nothing... How do I handle this?
+# is there a way to return a maybe(Card)? What would that be called?
+func get_hovered_card() -> Card:
+	if not hovered_cards: return
+	var highest_index = func(max, val): return val if val.get_index() > max.get_index() else max
+	var rightmost_card = hovered_cards.reduce(highest_index)
+	return rightmost_card
 
 # this is temporary testing code
 func _input(event):
@@ -65,8 +111,10 @@ func _input(event):
 		var card_to_add = CardScene.instantiate()
 		add_child(card_to_add)
 		card_to_add.position = CARD_SPAWN_POS
+		card_to_add.mouse_entered.connect(_on_mouse_entered_card)
+		card_to_add.mouse_exited.connect(_on_mouse_exited_card)
 
-func _compute_pos(hand_ratio : float, card_spread_x : int) -> Vector2:
+func _compute_pos(hand_ratio: float, card_spread_x: int) -> Vector2:
 	var relative_x = horizontal_spread_curve.sample(hand_ratio) * card_spread_x
 	var relative_y = vertical_spread_curve.sample(hand_ratio) * CARD_SPREAD_Y * -1
 	return Vector2(relative_x, relative_y)
