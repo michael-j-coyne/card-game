87abb9bfc6e6653fb2c363424a40ea816f6c3a5f
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2023-12-13 12:50:01 -0500

    Delete unused code in Card.gd

--- Card.gd
+++ Card.gd
@@ -1,15 +1,8 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
 func get_width() -> int:
 	return $Background.texture.get_width() * $Background.scale.x
 
 func get_height() -> int:
 	return $Background.texture.get_height() * $Background.scale.y
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
