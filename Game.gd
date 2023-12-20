62b9577414ac703dc92220e8bb1cf9b268cbbde8
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2023-12-19 19:25:57 -0500

    Implement adding cards to any play zone from hand

--- Game.gd
+++ Game.gd
@@ -9,12 +9,19 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func _p1_zone_clicked(zone: PlayZone):
+	
+func _zone_clicked(zone: PlayZone, player: String):
 	var hand = get_node("Hand")
 	var card : Card = hand.get_selected_card()
 	if card:
 		var new_card := CardScene.instantiate()
 		new_card.init(card.get_card_name())
 		hand.remove_card(card)
-		zone.add_card(new_card, "P1")
+		zone.add_card(new_card, player)
+
+func _p1_zone_clicked(zone: PlayZone):
+	_zone_clicked(zone, "P1")
+
+func _p2_zone_clicked(zone: PlayZone):
+	_zone_clicked(zone, "P2")
+	
