667abe2603dca512175eacb1f719becd201848f4
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2023-12-27 12:49:26 -0500

    Implement add_card func

--- play_zone/play_zone.gd
+++ play_zone/play_zone.gd
@@ -3,9 +3,21 @@ extends Node
 var p1_cards: Array[Card] = []
 var p2_cards: Array[Card] = []
 
+var _cards: Dictionary = {
+	Enums.Player.P1: p1_cards,
+	Enums.Player.P2: p2_cards
+}
+
+func _get_card_arr(player: Enums.Player) -> Array[Card]:
+	assert(player in _cards)
+	return _cards[player]
+
 # 
 func _sum_cards(cards: Array[Card]) -> int:
 	return cards.reduce(func(acc, card): return acc + card.get_value(), 0)
 
 func get_score() -> Score:
 	return Score.new(_sum_cards(p1_cards), _sum_cards(p2_cards))
+
+func add_card(card: Card, player: Enums.Player) -> void:
+	_get_card_arr(player).append(card)
